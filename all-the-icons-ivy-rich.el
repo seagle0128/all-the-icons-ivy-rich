b4226091a49c39f3b92d6ae8019a7a3b8d8a772d
Vincent Zhang <seagle0128@gmail.com>
2020-06-10 11:41:05 +0800

    Now ivy-rich only support 25+.

--- all-the-icons-ivy-rich.el
+++ all-the-icons-ivy-rich.el
@@ -4,8 +4,8 @@
 
 ;; Author: Vincent Zhang <seagle0128@gmail.com>
 ;; Homepage: https://github.com/seagle0128/all-the-icons-ivy-rich
-;; Version: 1.3.0
-;; Package-Requires: ((emacs "24.5") (ivy-rich "0.1.0") (all-the-icons "2.2.0"))
+;; Version: 1.3.1
+;; Package-Requires: ((emacs "25.1") (ivy-rich "0.1.0") (all-the-icons "2.2.0"))
 ;; Keywords: convenience, icons, ivy
 
 ;; This file is not part of GNU Emacs.
