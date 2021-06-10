aa4fee6455fd0d95217de4891de83d85ac9eb773
Vincent Zhang <seagle0128@gmail.com>
2021-06-11 03:34:13 +0800

    Use file-attribute-type.

--- all-the-icons-ivy-rich.el
+++ all-the-icons-ivy-rich.el
@@ -577,7 +577,7 @@ Display the true name when the file is a symlink."
   (let ((file (if (ivy--dirname-p candidate)
                   (propertize candidate 'face 'ivy-subdir)
                 candidate))
-        (type (nth 0 (file-attributes (expand-file-name candidate ivy--directory)))))
+        (type (file-attribute-type (file-attributes (expand-file-name candidate ivy--directory)))))
     (if (stringp type)
         (concat file
                 (propertize (concat " -> " type)
