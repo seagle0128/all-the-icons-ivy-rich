328047e1b107baa1f7df3c706000a1ec9e8f51db
Vincent Zhang <seagle0128@gmail.com>
2022-03-10 19:12:51 +0800

    Bump version.

--- all-the-icons-ivy-rich.el
+++ all-the-icons-ivy-rich.el
@@ -4,7 +4,7 @@
 
 ;; Author: Vincent Zhang <seagle0128@gmail.com>
 ;; Homepage: https://github.com/seagle0128/all-the-icons-ivy-rich
-;; Version: 1.7.0
+;; Version: 1.7.1
 ;; Package-Requires: ((emacs "25.1") (ivy-rich "0.1.0") (all-the-icons "2.2.0"))
 ;; Keywords: convenience, icons, ivy
 
