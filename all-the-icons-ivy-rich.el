56f771a827027270e93415edd29cba52dba424a6
Vincent Zhang <seagle0128@gmail.com>
2022-04-07 14:26:56 +0800

    [Enhancement] Improve project commands.

--- all-the-icons-ivy-rich.el
+++ all-the-icons-ivy-rich.el
@@ -606,7 +606,11 @@ This value is adjusted depending on the `window-width'."
     counsel-projectile-switch-project
     (:columns
      ((all-the-icons-ivy-rich-file-icon)
-      (all-the-icons-ivy-rich-project-name (:width 0.4)))
+      (all-the-icons-ivy-rich-project-name (:width 0.4))
+      (all-the-icons-ivy-rich-project-file-id (:width 15 :face all-the-icons-ivy-rich-file-owner-face :align right))
+      (all-the-icons-ivy-rich-project-file-modes (:width 12))
+      (all-the-icons-ivy-rich-project-file-size (:width 7 :face all-the-icons-ivy-rich-size-face))
+      (all-the-icons-ivy-rich-project-file-modification-time (:face all-the-icons-ivy-rich-time-face)))
      :delimiter "\t")
     counsel-projectile-switch-to-buffer
     (:columns
@@ -623,7 +627,7 @@ This value is adjusted depending on the `window-width'."
      :delimiter "\t")
     counsel-projectile-find-dir
     (:columns
-     ((all-the-icons-ivy-rich-dir-icon)
+     ((all-the-icons-ivy-rich-file-icon)
       (counsel-projectile-find-dir-transformer (:width 0.4))
       (all-the-icons-ivy-rich-project-file-id (:width 15 :face all-the-icons-ivy-rich-file-owner-face :align right))
       (all-the-icons-ivy-rich-project-file-modes (:width 12))
@@ -664,6 +668,17 @@ This value is adjusted depending on the `window-width'."
      ((all-the-icons-ivy-rich-function-icon)
       (counsel-M-x-transformer (:width 0.3))
       (ivy-rich-counsel-function-docstring (:face all-the-icons-ivy-rich-doc-face))))
+
+    ;; projectile
+    projectile-completing-read
+    (:columns
+     ((all-the-icons-ivy-rich-file-icon)
+      (all-the-icons-ivy-rich-project-find-file-transformer (:width 0.4))
+      (all-the-icons-ivy-rich-project-file-id (:width 15 :face all-the-icons-ivy-rich-file-owner-face :align right))
+      (all-the-icons-ivy-rich-project-file-modes (:width 12))
+      (all-the-icons-ivy-rich-project-file-size (:width 7 :face all-the-icons-ivy-rich-size-face))
+      (all-the-icons-ivy-rich-project-file-modification-time (:face all-the-icons-ivy-rich-time-face)))
+     :delimiter "\t")
 
     ;; project
     project-switch-project
@@ -873,13 +888,21 @@ This value is adjusted depending on the `window-width'."
     lsp-ivy-workspace-folders-remove
     (:columns
      ((all-the-icons-ivy-rich-dir-icon)
-      (all-the-icons-ivy-rich-project-name))
+      (all-the-icons-ivy-rich-project-name (:width 0.4))
+      (all-the-icons-ivy-rich-project-file-id (:width 15 :face all-the-icons-ivy-rich-file-owner-face :align right))
+      (all-the-icons-ivy-rich-project-file-modes (:width 12))
+      (all-the-icons-ivy-rich-project-file-size (:width 7 :face all-the-icons-ivy-rich-size-face))
+      (all-the-icons-ivy-rich-project-file-modification-time (:face all-the-icons-ivy-rich-time-face)))
      :delimiter "\t")
 
     treemacs-projectile
     (:columns
      ((all-the-icons-ivy-rich-dir-icon)
-      (all-the-icons-ivy-rich-project-name))
+      (all-the-icons-ivy-rich-project-name (:width 0.4))
+      (all-the-icons-ivy-rich-project-file-id (:width 15 :face all-the-icons-ivy-rich-file-owner-face :align right))
+      (all-the-icons-ivy-rich-project-file-modes (:width 12))
+      (all-the-icons-ivy-rich-project-file-size (:width 7 :face all-the-icons-ivy-rich-size-face))
+      (all-the-icons-ivy-rich-project-file-modification-time (:face all-the-icons-ivy-rich-time-face)))
      :delimiter "\t"))
   "Definitions for ivy-rich transformers.
 
